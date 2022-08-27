6dfcb7c2a82a0cb324d3a9be708eb5bcda3603bf
Faisalm <feisalmohammadi@gmail.com>
2022-08-27 22:49:07 +0200

    neue methoden hinzugefügt,
    um die anzahl der bücher des bistimmten bandes in shared preferenses zu speichern

--- lib/features/shopping/provider/shopping_card_service.dart
+++ lib/features/shopping/provider/shopping_card_service.dart
@@ -6,6 +6,7 @@ import 'package:waf_code_challenge/features/models/book_model.dart';
 @singleton
 class ShoppingCardService with ChangeNotifier {
   int _shoppingCardCounter = 0;
+  int _shoppingCardListItemCounter = 0;
   int get shoppingCardCounter => _shoppingCardCounter;
 
   List<BookModel> _shoppingCardItems = [];
@@ -32,6 +33,24 @@ class ShoppingCardService with ChangeNotifier {
     notifyListeners();
   }
 
+  int getShoppingCardListItemCounter() {
+    _getPreferencesItems();
+    return _shoppingCardListItemCounter;
+  }
+
+  void addShoppingCardListItemCounter() {
+    _shoppingCardListItemCounter++;
+    _setPreferencesItems();
+    notifyListeners();
+  }
+  void subtractShoppingCardListItemCounter() {
+    _shoppingCardListItemCounter--;
+    _setPreferencesItems();
+    notifyListeners();
+  }
+
+
+
   void _getPreferencesItems() {
     if (AppPreferencesAndSecureStorage.getShoppingCardCounter() != null) {
       _shoppingCardCounter =
@@ -40,10 +59,14 @@ class ShoppingCardService with ChangeNotifier {
     if (AppPreferencesAndSecureStorage.getBooksInfo() != null) {
       _shoppingCardItems = AppPreferencesAndSecureStorage.getBooksInfo()!;
     }
+    if (AppPreferencesAndSecureStorage.getShoppingCardListItemCounter() != null) {
+      _shoppingCardListItemCounter = AppPreferencesAndSecureStorage.getShoppingCardListItemCounter()!;
+    }
   }
 
   void _setPreferencesItems() {
     AppPreferencesAndSecureStorage.setShoppingCardCounter(_shoppingCardCounter);
     AppPreferencesAndSecureStorage.setBookInfo(_shoppingCardItems);
+    AppPreferencesAndSecureStorage.setShoppingCardListItemCounter(_shoppingCardListItemCounter);
   }
 }
